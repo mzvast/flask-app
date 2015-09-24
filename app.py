2f71da9d873719f5773fbfc14b500bbe259b04c4
mzvast <mzvast@gmail.com>
2015-09-24 09:29:03 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World, Flask is working!"
+
+app.run(host = os.getenv('IP','0.0.0.0'), port = int(os.getenv('PORT',8080)))
+
+if __name__ == "__main__":
+    app.run()
+    app.debug = True
